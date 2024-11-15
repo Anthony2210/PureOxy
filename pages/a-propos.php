8d2925ecba541f7f3069641b00f7454d3a53aa4e
Anthony2210 <antocombague@gmail.com>
2024-11-15 10:18:22 +0100

    correction bug

--- pages/a-propos.php
+++ pages/a-propos.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+ob_start();
+include '../includes/header.php';
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <meta charset="UTF-8">
@@ -6,8 +11,7 @@
 <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="../styles/includes.css">
 
-<?php include '../includes/header.php'; ?>
-
+<main>
 <section id="apropos">
     <h2>À propos de PureOxy</h2>
     <p>
@@ -15,11 +19,12 @@
         Nous fournissons des informations en temps réel sur les niveaux de pollution atmosphérique et proposons des solutions pour aider à améliorer la qualité de l'air.
     </p>
 </section>
+</main>
+
 <!-- Appel à l'action -->
 <section id="cta" class="mt-5">
     <h2>Nos articles</h2>
     <a href="../pages/qualite_air.php" class="button">Sources de pollution et effets sur la santé</a>
     <a href="lutte_pollution.php" class="button">Lutte contre la pollution de l'air</a>
 </section>
-
 <?php include '../includes/footer.php'; ?>
